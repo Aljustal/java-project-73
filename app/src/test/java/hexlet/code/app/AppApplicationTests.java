7f9fc5fa9331d3d09caba5547083d558ff3236eb
Aljustal <orbit.fsk@yandex.ru>
2023-01-21 21:13:03 +0300

    add application.properties for dev & prod

--- app/src/test/java/hexlet/code/app/AppApplicationTests.java
+++ app/src/test/java/hexlet/code/app/AppApplicationTests.java
@@ -3,11 +3,18 @@ package hexlet.code.app;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 @SpringBootTest
 class AppApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void testInit() {
+		assertThat(true).isTrue();
 	}
 
 }
