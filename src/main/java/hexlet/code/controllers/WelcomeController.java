fa329ccf7629ed3c98b612653e32af74e20b94b1
Aljustal <orbit.fsk@yandex.ru>
2023-01-24 14:35:14 +0300

    add checkstyle config

--- src/main/java/hexlet/code/controllers/WelcomeController.java
+++ src/main/java/hexlet/code/controllers/WelcomeController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class WelcomeController {
 
-    @GetMapping("/")
+    @GetMapping("/welcome")
     public String welcome() {
         System.out.println("Welcome to Spring");
         return "Welcome to Spring";
