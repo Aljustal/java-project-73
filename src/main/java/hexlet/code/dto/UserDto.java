1f7f074184fbf3524fceec5e1023d24143d72a32
Aljustal <orbit.fsk@yandex.ru>
2023-01-26 04:06:47 +0300

    add UserDto

---
+++ src/main/java/hexlet/code/dto/UserDto.java
@@ -0,0 +1,33 @@
+package hexlet.code.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Getter
+@Setter
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDto {
+
+    @NotBlank
+    private String firstName;
+
+    @NotBlank
+    private String lastName;
+
+    @NotBlank
+    @Email
+    private String email;
+
+    @NotBlank
+    @Size(min = 3, max = 100)
+    private String password;
+}
