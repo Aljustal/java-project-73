354fafa12e187e58a5a1ca348e3980c8e86f5b87
Aljustal <orbit.fsk@yandex.ru>
2023-01-26 04:07:23 +0300

    add User.java

--- src/main/java/hexlet/code/model/User.java
+++ src/main/java/hexlet/code/model/User.java
@@ -6,12 +6,12 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import org.hibernate.annotations.CreationTimestamp;
-import org.springframework.data.annotation.Id;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import static javax.persistence.TemporalType.TIMESTAMP;
